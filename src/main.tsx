ff1e8fd1040956251c9a5c8b89557e14eab1cbea
AmIMaybeAnOrange <89079795+AmIMaybeAnOrange@users.noreply.github.com>
2025-12-29 10:45:50 -0400

    Remove duplicate leaflet.css import
    Removed duplicate import of leaflet.css.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 
+import 'leaflet/dist/leaflet.css';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'leaflet/dist/leaflet.css';
 
 // Remove dark mode class addition
 createRoot(document.getElementById("root")!).render(<App />);
